03c9bca17b2ed9b3842f4ad8eb61f6de5d1e7b88
AsamiShingo <s_asami@cosmoroot.co.jp>
2021-10-31 20:20:47 +0900

    Webのフォルダを管理するクラスを作成しリファクタリング実施

--- web/keiyakuweb.py
+++ web/keiyakuweb.py
@@ -11,62 +11,95 @@ import json
 import numpy as np
 import datetime
 from keiyakudata import KeiyakuData
-from keiyakumodel import KeiyakuModel
 from keiyakumodelfactory import KeiyakuModelFactory
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), r"data")
 ANALYZE_DIR = os.path.join(os.path.dirname(__file__), r"analyze")
-create_seqid_mutex = threading.Lock()
 keiyaku_analyze_mutex = threading.Lock()
 
-def keiyaku_analyze_txt(seqid):
-    targetdir = os.path.join(DATA_DIR, seqid)    
-    with open(os.path.join(targetdir, "param.json"), "r") as parafile:
-        paradata = json.load(parafile)
-        txtname = paradata["txtname"]
-        csvname = paradata["csvname"]
+class KeiyakuWebData:
     
-    csvpath = os.path.join(targetdir, csvname)
-    keiyakudata = KeiyakuData(csvpath)
-    datas = keiyakudata.get_datas()
-    scores1, scores2 = keiyaku_analyze(csvpath)
+    PARA_FILE = "param.json"
 
-    analyze_dir = os.path.join(ANALYZE_DIR, seqid)
-    os.makedirs(analyze_dir, exist_ok=True)
-    analyze_file = os.path.join(analyze_dir, datetime.datetime.now().strftime('%Y%m%d%H%M%S') + ".txt")
+    create_seqid_mutex = threading.Lock()        
 
-    np.set_printoptions(precision=2, floatmode='fixed')
+    def __init__(self, seqid=None, orgfilename="", mimetype=""):
+        if seqid != None:
+            self.seqid = seqid
+            self.paradata = {}
 
-    with open(analyze_file, "w") as f:
-        for data, score1, score2 in zip(datas, scores1, scores2):
-            sentense = data[6]
-            kind1 = score2.argmax()
-            if score1 >= 0.5:
-                f.write("{}---------------------------------------------\n".format(score1))
-                
-            f.write("{}-{:0.2f}:{}\n".format(kind1, score2[kind1], sentense))
-    
-    return analyze_file, txtname
+            with open(os.path.join(self.get_dirpath(), self.PARA_FILE), "r") as parafile:
+                para = json.load(parafile)
+                self.paradata["orgfilename"] = para["orgfilename"]
+                self.paradata["filename"] = para["filename"]
+                self.paradata["txtname"] = para["txtname"]
+                self.paradata["csvname"] = para["csvname"]
+                self.paradata["mimetype"] = para["mimetype"]
+        else:
+            if orgfilename == "" or mimetype == "":
+                raise AttributeError()
 
-def keiyaku_analyze_json(seqid):
-    targetdir = os.path.join(DATA_DIR, seqid)    
-    with open(os.path.join(targetdir, "param.json"), "r") as parafile:
-        paradata = json.load(parafile)
-        csvname = paradata["csvname"]
-    
-    csvpath = os.path.join(targetdir, csvname)
-    scores1, scores2 = keiyaku_analyze(csvpath)
-    
-    jsondata = {}
-    for col, score in enumerate(zip(scores1, scores2)):
-        score1 = score[0]
-        score2 = score[1]
-        scoredata = {}
-        scoredata[1] = round(float(score1[0]), 2)
-        scoredata[2] = { i:round(float(score), 2) for i, score in enumerate(score2) }
-        jsondata[col] = scoredata
-        
-    return jsondata
+            self.seqid = self._create_seqid()
+            self.paradata = {}
+            
+            filename = secure_filename(orgfilename)
+            self.paradata["orgfilename"] = orgfilename
+            self.paradata["filename"] = filename
+            self.paradata["txtname"] = "txt_{}.txt".format(os.path.basename(filename))
+            self.paradata["csvname"] = "csv_{}.csv".format(os.path.basename(filename))
+            self.paradata["mimetype"] = mimetype
+            with open(os.path.join(self.get_dirpath(), self.PARA_FILE), "w") as parafile:
+                json.dump(self.paradata, parafile, ensure_ascii=False, indent=4)
+
+    def get_dirpath(self):
+        return os.path.join(DATA_DIR, self.seqid)
+
+    def get_orgfilename(self):
+        return self.paradata["orgfilename"]
+
+    def get_orgtxtname(self):
+        return os.path.basename(self.get_orgfilename()) + ".txt"
+
+    def get_filename(self):
+        return self.paradata["filename"]
+
+    def get_filepath(self):
+        return os.path.join(self.get_dirpath(), self.get_filename())
+
+    def get_txtname(self):
+        return self.paradata["txtname"]
+
+    def get_txtpath(self):
+        return os.path.join(self.get_dirpath(), self.get_txtname())
+
+    def get_csvname(self):
+        return self.paradata["csvname"]
+
+    def get_csvpath(self):
+        return os.path.join(self.get_dirpath(), self.get_csvname())
+
+    def get_mimetype(self):
+        return self.paradata["mimetype"]
+
+    def create_analyzepath(self):
+        analyze_dir = os.path.join(ANALYZE_DIR, self.seqid)
+        os.makedirs(analyze_dir, exist_ok=True)
+        return os.path.join(analyze_dir, datetime.datetime.now().strftime('%Y%m%d%H%M%S') + ".txt")
+
+    def _create_seqid(self):
+        self.create_seqid_mutex.acquire()
+
+        dirs = glob.glob(os.path.join(DATA_DIR, r"?????"))
+        dirs = [os.path.basename(dir) for dir in dirs if os.path.isdir(dir)]
+        seqs = [int(seq) for seq in dirs if re.match(r'[0-9]{5}', seq)]
+        seq = str(max(seqs) + 1) if len(seqs) != 0 else "0"
+
+        seqid = seq.zfill(5)
+        os.mkdir(os.path.join(DATA_DIR, seqid))
+
+        self.create_seqid_mutex.release()
+
+        return seqid
 
 def keiyaku_analyze(csvpath):
     keiyaku_analyze_mutex.acquire()
@@ -80,79 +113,6 @@ def keiyaku_analyze(csvpath):
     
     return score1, score2
 
-def create_seqid():
-    create_seqid_mutex.acquire()
-
-    dirs = glob.glob(os.path.join(DATA_DIR, r"?????"))
-    dirs = [os.path.basename(dir) for dir in dirs if os.path.isdir(dir)]
-    seqs = [int(seq) for seq in dirs if re.match(r'[0-9]{5}', seq)]
-
-    if len(seqs) != 0:
-        seq = str(max(seqs) + 1)
-    else:
-        seq = "0"
-
-    seqid = seq.zfill(5)
-    os.mkdir(os.path.join(DATA_DIR, seqid))
-
-    create_seqid_mutex.release()
-
-    return seqid
-
-def get_download_filepath(seqid):
-    targetdir = os.path.join(DATA_DIR, seqid)
-    
-    with open(os.path.join(targetdir, "param.json"), "r") as parafile:
-        paradata = json.load(parafile)
-        filepath = os.path.join(targetdir, paradata["filename"])
-
-    return filepath
-
-def delete_seqid(seqid):
-    dir = os.path.join(DATA_DIR, seqid)
-    if os.path.isdir(dir) == False:
-        raise FileNotFoundError("{}のディレクトリが存在しません".format(seqid))
-
-    for delfile in os.listdir(dir):
-        os.remove(os.path.join(dir, delfile))
-
-    os.rmdir(dir)
-
-def save_upload_file(seqid, request):
-    f = request.files["file"]
-    targetdir = os.path.join(DATA_DIR, seqid)
-
-    uploadsavename = secure_filename(f.filename)
-    txtname = "txt_{}.txt".format(os.path.basename(uploadsavename))
-    csvname = "csv_{}.csv".format(os.path.basename(uploadsavename))
-    uploadsavepath = os.path.join(targetdir, uploadsavename)
-    txtpath = os.path.join(targetdir, txtname)
-    csvpath = os.path.join(targetdir, csvname)
-    f.save(uploadsavepath)
-    KeiyakuData.create_keiyaku_data(uploadsavepath, txtpath, csvpath)
-
-    with open(os.path.join(targetdir, "param.json"), "w") as parafile:
-        data = {}
-        data["filename"] = uploadsavename
-        data["txtname"] = txtname
-        data["csvname"] = csvname
-        json.dump(data, parafile, ensure_ascii=False, indent=4)
-
-def get_datas():
-    datas = []
-    dirs = glob.glob(os.path.join(DATA_DIR, r"?????"))
-    for dir in dirs:
-        filename = ""
-        seqid = os.path.basename(dir)
-        with open(os.path.join(dir, "param.json"), "r") as parafile:
-            paradata = json.load(parafile)
-            filename = paradata["filename"]
-
-        if filename != "":
-            datas.append((seqid, filename))
-
-    return datas
-
 app = Flask(__name__)
 
 def init_web(debugmode):
@@ -163,35 +123,84 @@ def init_web(debugmode):
     
 @app.route("/keiyaku_group/")
 def index():
-    datas = get_datas()
+    datas = []
+    for dir in glob.glob(os.path.join(DATA_DIR, r"?????")):
+        seqid = os.path.basename(dir)
+        data = KeiyakuWebData(seqid)
+
+        if os.path.isfile(data.get_filepath()):
+            datas.append((seqid, data.get_orgfilename()))
+
     return render_template("index.html", datas=datas)
 
 @app.route("/keiyaku_group/upload", methods=["POST"])
 def upload():
-    seqid = create_seqid()
-    save_upload_file(seqid, request)
+    f = request.files["file"]
+    data = KeiyakuWebData(orgfilename=f.filename, mimetype=request.mimetype)
+    
+    f.save(data.get_filepath())
+    KeiyakuData.create_keiyaku_data(data.get_filepath(), data.get_txtpath(), data.get_csvpath())
+
     return redirect(url_for("index"))
 
 @app.route("/keiyaku_group/download", methods=["POST"])
 def download():
     seqid = request.form["seqid"]
-    filepath = get_download_filepath(seqid)
-    return send_file(filepath, as_attachment=True, attachment_filename=os.path.basename(filepath))
+    data = KeiyakuWebData(seqid)
+    return send_file(data.get_filepath(), as_attachment=True, attachment_filename=data.get_orgfilename())
 
 @app.route("/keiyaku_group/delete", methods=["POST"])
 def delete():
     seqid = request.form["seqid"]
-    delete_seqid(seqid)
+    data = KeiyakuWebData(seqid)
+
+    dirpath = data.get_dirpath()
+    if os.path.isdir(dirpath) == False:
+        raise FileNotFoundError("{}のディレクトリが存在しません".format(dirpath))
+
+    for delfile in os.listdir(dirpath):
+        os.remove(os.path.join(dirpath, delfile))
+
+    os.rmdir(dirpath)
+
     return redirect(url_for("index"))
 
 @app.route("/keiyaku_group/analyze_txt", methods=["POST"])
 def analyze_txt():
     seqid = request.form["seqid"]
-    filepath, filename = keiyaku_analyze_txt(seqid)
-    return send_file(filepath, as_attachment=True, attachment_filename=filename)
+    data = KeiyakuWebData(seqid)
+    
+    scores1, scores2 = keiyaku_analyze(data.get_csvpath())
+    keiyakudata = KeiyakuData(data.get_csvpath())
+    sentensedatas = keiyakudata.get_datas()
+    analyze_path = data.create_analyzepath()
+
+    np.set_printoptions(precision=2, floatmode='fixed')
+    with open(analyze_path, "w") as f:
+        for sentensedata, score1, score2 in zip(sentensedatas, scores1, scores2):
+            sentense = sentensedata[6]
+            kind1 = score2.argmax()
+            if score1 >= 0.5:
+                f.write("{}---------------------------------------------\n".format(score1))
+                
+            f.write("{}-{:0.2f}:{}\n".format(kind1, score2[kind1], sentense))
+
+    return send_file(analyze_path, as_attachment=True, attachment_filename=data.get_orgtxtname())
 
 @app.route("/keiyaku_group/analyze_json", methods=["POST"])
 def analyze_json():
     seqid = request.form["seqid"]
-    jsondata = keiyaku_analyze_json(seqid)
+    data = KeiyakuWebData(seqid)
+
+    scores1, scores2 = keiyaku_analyze(data.get_csvpath())
+    
+    jsondata = {}
+    for col, score in enumerate(zip(scores1, scores2)):
+        score1 = score[0]
+        score2 = score[1]
+        scoredata = {}
+        scoredata[1] = round(float(score1[0]), 2)
+        scoredata[2] = { i:round(float(score), 2) for i, score in enumerate(score2) }
+        jsondata[col] = scoredata
+
     return jsonify(jsondata)
